f0059c838bb3e6d1cfb2388b27df808f0aa7aa12
Robin Bardet-Tomczak <rbardet-@student.42lehavre.fr>
2025-03-24 23:24:03 +0100

    Feature(minimap): resize minimap and added door

--- srcs/rendering/minimap.c
+++ srcs/rendering/minimap.c
@@ -6,11 +6,29 @@
 /*   By: rbardet- <rbardet-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/24 20:10:20 by rbardet-          #+#    #+#             */
-/*   Updated: 2025/03/24 23:04:21 by rbardet-         ###   ########.fr       */
+/*   Updated: 2025/03/24 23:22:22 by rbardet-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../cub3d.h"
+
+void	draw_square(t_cub *cub, int x, int y, int color)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	while (i < 9)
+	{
+		j = 0;
+		while (j < 9)
+		{
+			mlx_pixel_put(cub->init_ptr, cub->win_ptr, x + j, y + i, color);
+			j++;
+		}
+		i++;
+	}
+}
 
 void	render_minimap(t_cub *cub)
 {
@@ -24,20 +42,15 @@ void	render_minimap(t_cub *cub)
 		while (cub->map[i][j])
 		{
 			if (cub->map[i][j] == '1')
-			{
-				mlx_pixel_put(cub->init_ptr, cub->win_ptr, j * 1,
-					i * 1, 0xFF0000);
-			}
+				draw_square(cub, j * 9, i * 9, 0xFF0000);
+			else if (cub->map[i][j] == 'D')
+				draw_square(cub, j * 9, i * 9, 0x2BFAFA);
 			else if (cub->map[i][j] == cub->spawn_view)
-			{
-				mlx_pixel_put(cub->init_ptr, cub->win_ptr, j * 1,
-					i * 1, 0x00FF00);
-			}
+				draw_square(cub, j * 9, i * 9, 0x00FF00);
 			j++;
 		}
 		i++;
 	}
-	return ;
 }
 
 int	minimap(t_cub *cub)
