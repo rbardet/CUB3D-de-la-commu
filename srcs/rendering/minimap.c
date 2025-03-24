8b2bf392e520082272e7aa272ddfdda8776daf67
Robin Bardet-Tomczak <rbardet-@student.42lehavre.fr>
2025-03-24 23:14:00 +0100

    Feature(minimap): Added player to minimap

--- srcs/rendering/minimap.c
+++ srcs/rendering/minimap.c
@@ -6,7 +6,7 @@
 /*   By: rbardet- <rbardet-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/24 20:10:20 by rbardet-          #+#    #+#             */
-/*   Updated: 2025/03/24 23:00:33 by rbardet-         ###   ########.fr       */
+/*   Updated: 2025/03/24 23:04:21 by rbardet-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,6 +27,11 @@ void	render_minimap(t_cub *cub)
 			{
 				mlx_pixel_put(cub->init_ptr, cub->win_ptr, j * 1,
 					i * 1, 0xFF0000);
+			}
+			else if (cub->map[i][j] == cub->spawn_view)
+			{
+				mlx_pixel_put(cub->init_ptr, cub->win_ptr, j * 1,
+					i * 1, 0x00FF00);
 			}
 			j++;
 		}
