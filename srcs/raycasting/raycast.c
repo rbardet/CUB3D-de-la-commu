5063ffd7f743947430ff1f2d9868c4b9312b7895
Robin Bardet-Tomczak <rbardet-@student.42lehavre.fr>
2025-03-25 22:06:38 +0100

    Fixed: raycasting

--- srcs/raycasting/raycast.c
+++ srcs/raycasting/raycast.c
@@ -6,20 +6,23 @@
 /*   By: rbardet- <rbardet-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/24 21:19:38 by hdelacou          #+#    #+#             */
-/*   Updated: 2025/03/25 21:57:58 by rbardet-         ###   ########.fr       */
+/*   Updated: 2025/03/25 22:06:17 by rbardet-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../cub3d.h"
 #include <stdio.h>
 #include <math.h>
+
 void putPixel(int x, int y, t_rgb color, t_cub *cub)
 {
 	int pixelIndex;
 
 	if (x >= 0 && x < cub->win_width && y >= 0 && y < cub->win_height)
 	{
-		pixelIndex = (y * cub->win_width + x) * 4;
+		// Calculer l'adresse du pixel à l'emplacement (x, y) dans le buffer
+		pixelIndex = (y * cub->win_width + x) * 4; // 4 bytes par pixel (RGBA)
+
 		cub->img.addr[pixelIndex] = color.blue;
 		cub->img.addr[pixelIndex + 1] = color.green;
 		cub->img.addr[pixelIndex + 2] = color.red;
@@ -28,106 +31,136 @@ void putPixel(int x, int y, t_rgb color, t_cub *cub)
 
 void verLine(int x, int drawStart, int drawEnd, t_rgb color, t_cub *cub)
 {
-	if (drawStart < 0) drawStart = 0;
-	if (drawEnd >= cub->win_height) drawEnd = cub->win_height - 1;
+	int y;
+	// Limiter les valeurs de y pour éviter de dépasser la taille de l'écran
+	if (drawStart < 0)
+		drawStart = 0;
+	if (drawEnd >= cub->win_height)
+		drawEnd = cub->win_height - 1;
 
-	for (int y = drawStart; y <= drawEnd; y++)
+	// Initialiser le compteur pour les pixels à dessiner
+	y = drawStart;
+	// Dessiner les pixels avec une boucle while
+	while (y <= drawEnd)
 	{
-		putPixel(x, y, color, cub);
+		putPixel(x, y, color, cub); // Dessiner le pixel à la position (x, y)
+		y++;  // Passer au pixel suivant
 	}
 }
 
 int raycast(t_cub *cub)
 {
-	for (int x = 0; x < cub->win_width; x++)
+	int x;
+	double cameraX;
+	double rayDirX;
+	double rayDirY;
+	double posX;
+	double posY;
+	double sideDistX;
+	double sideDistY;
+	double deltaDistX;
+	double deltaDistY;
+	double perpWallDist;
+	int mapX;
+	int mapY;
+	int stepX;
+	int stepY;
+	int side;
+	int hit;
+	int lineHeight;
+	int drawStart;
+	int drawEnd;
+	t_rgb color;
+
+	hit = 0;  // Initialize hit
+	x = 0;    // Initialize x
+	posX = cub->player.pos_x;
+	posY = cub->player.pos_y;
+
+	while (x < cub->win_width) // Loop for the entire screen width
 	{
-		// Calculate ray position and direction
-		double cameraX = 2 * x / (double)cub->win_width - 1;
-		double rayDirX = cub->player.dir_x + cub->player.plane_x * cameraX;
-		double rayDirY = cub->player.dir_y + cub->player.plane_y * cameraX;
+		cameraX = 2 * x / (double)cub->win_width - 1;
+		rayDirX = cub->player.dir_x + cub->player.plane_x * cameraX;
+		rayDirY = cub->player.dir_y + cub->player.plane_y * cameraX;
+		mapX = (int)posX;
+		mapY = (int)posY;
 
-		// Player's position with fractional part
-		double posX = cub->player.pos_x;
-		double posY = cub->player.pos_y;
+		deltaDistX = fabs(1.0 / rayDirX);
+		deltaDistY = fabs(1.0 / rayDirY);
 
-		// Map coordinates
-		int mapX = (int)posX;
-		int mapY = (int)posY;
 
-		// Calculate step and initial side distances
-		double deltaDistX = (rayDirX == 0) ? 1e30 : fabs(1 / rayDirX);
-		double deltaDistY = (rayDirY == 0) ? 1e30 : fabs(1 / rayDirY);
-
-		int stepX;
-		int stepY;
-		double sideDistX;
-		double sideDistY;
-
-		if (rayDirX < 0) {
+		// Set steps and sideDist for ray direction
+		if (rayDirX < 0)
+		{
 			stepX = -1;
 			sideDistX = (posX - mapX) * deltaDistX;
-		} else {
+		}
+		else
+		{
 			stepX = 1;
 			sideDistX = (mapX + 1.0 - posX) * deltaDistX;
 		}
 
-		if (rayDirY < 0) {
+		if (rayDirY < 0)
+		{
 			stepY = -1;
 			sideDistY = (posY - mapY) * deltaDistY;
-		} else {
+		}
+		else
+		{
 			stepY = 1;
 			sideDistY = (mapY + 1.0 - posY) * deltaDistY;
 		}
 
-		// DDA algorithm
-		int hit = 0;
-		int side;
-		while (!hit)
+		// Debugging prints: Display ray parameters
+		printf("Ray %d: posX = %f, posY = %f\n", x, posX, posY);
+		printf("Ray %d: rayDirX = %f, rayDirY = %f\n", x, rayDirX, rayDirY);
+		printf("Ray %d: mapX = %d, mapY = %d\n", x, mapX, mapY);
+		printf("Ray %d: sideDistX = %f, sideDistY = %f\n", x, sideDistX, sideDistY);
+
+		hit = 0;
+		while (hit == 0)
 		{
-			if (sideDistX < sideDistY) {
+			if (sideDistX < sideDistY)
+			{
 				sideDistX += deltaDistX;
 				mapX += stepX;
-				side = 0;
-			} else {
+				side = 0; // X direction (horizontal)
+			}
+			else
+			{
 				sideDistY += deltaDistY;
 				mapY += stepY;
-				side = 1;
+				side = 1; // Y direction (vertical)
 			}
 
-			// Check map bounds
-			if (mapX < 0 || mapX >= cub->player.map_width || mapY < 0 || mapY >= cub->player.map_height)
-				break;
-
-			// Check if ray hit a wall
-			if (cub->map[mapY][mapX] > 0)  // Note: map[Y][X] instead of map[X][Y]
+			if (cub->map[mapX][mapY] > 0)
 				hit = 1;
 		}
 
-		// Calculate perpendicular wall distance
-		double perpWallDist;
+		// Calculate perpendicular wall distance and debug
 		if (side == 0)
-			perpWallDist = (mapX - posX + (1 - stepX) / 2) / rayDirX;
+			perpWallDist = (sideDistX - deltaDistX);
 		else
-			perpWallDist = (mapY - posY + (1 - stepY) / 2) / rayDirY;
+			perpWallDist = (sideDistY - deltaDistY);
 
-		// Prevent division by zero in line height calculation
-		perpWallDist = fabs(perpWallDist) + 1e-6;
+		printf("Ray %d: perpWallDist = %f\n", x, perpWallDist);
 
-		// Calculate line height and draw limits
-		int lineHeight = (int)(cub->win_height / perpWallDist);
-		int drawStart = -lineHeight / 2 + cub->win_height / 2;
-		drawStart = drawStart < 0 ? 0 : drawStart;
-		int drawEnd = lineHeight / 2 + cub->win_height / 2;
-		drawEnd = drawEnd >= cub->win_height ? cub->win_height - 1 : drawEnd;
+		lineHeight = (int)(cub->win_height / perpWallDist);
 
-		// Choose wall color based on side hit
-		t_rgb color;
+		drawStart = -lineHeight / 2 + cub->win_height / 2;
+		if (drawStart < 0)
+			drawStart = 0;
+		drawEnd = lineHeight / 2 + cub->win_height / 2;
+		if (drawEnd >= cub->win_height)
+			drawEnd = cub->win_height - 1;
+
 		if (side == 0)
-			color = (t_rgb){0, 120, 131};  // Horizontal wall
+			color = (t_rgb){0, 120, 131};
 		else
-			color = (t_rgb){43, 174, 90};  // Vertical wall
-
+			color = (t_rgb){43, 174, 90};
 		verLine(x, drawStart, drawEnd, color, cub);
+		x++;
 	}
 
 	mlx_put_image_to_window(cub->init_ptr, cub->win_ptr, cub->img_ptr, 0, 0);
