65d25ab19926ac1421e752aca480ebf3d45ca6e5
hugo <hugodelacour.pro@gmail.com>
2025-03-25 16:10:57 +0100

    fix: put DEFINE TILE for the tile size of minimap, MODULO !

--- srcs/cub3d.h
+++ srcs/cub3d.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   cub3d.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rbardet- <rbardet-@student.42.fr>          +#+  +:+       +#+        */
+/*   By: hdelacou <hdelacou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/19 23:51:13 by rbardet-          #+#    #+#             */
-/*   Updated: 2025/03/24 23:12:23 by rbardet-         ###   ########.fr       */
+/*   Updated: 2025/03/25 16:04:51 by hdelacou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,20 +17,22 @@
 #include <X11/X.h>
 #include <X11/keysym.h>
 #include <fcntl.h>
+#include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <unistd.h>
-#include <math.h>
 
-#define WIN_HEIGHT 1200
-#define WIN_WIDTH 900
+#define WIN_HEIGHT 1450
+#define WIN_WIDTH 1080
+
+#define Tile (WIN_HEIGHT / 120) % WIN_WIDTH
 
 #define GRAPH_HEIGHT 64
 #define GRAPH_WIDTH 64
 
-typedef int	t_bool;
+typedef int		t_bool;
 #define TRUE 1
 #define FALSE 0
 
@@ -39,20 +41,20 @@ typedef int	t_bool;
 
 typedef struct s_rgb
 {
-	int		red;
-	int		green;
-	int		blue;
-}			t_rgb;
+	int			red;
+	int			green;
+	int			blue;
+}				t_rgb;
 
 typedef struct s_player
 {
-	int		pos_x;
-	int		pos_y;
-	float	dir_x;
-	float	dir_y;
-	float	plane_x;
-	float	plane_y;
-}			t_player;
+	int			pos_x;
+	int			pos_y;
+	float		dir_x;
+	float		dir_y;
+	float		plane_x;
+	float		plane_y;
+}				t_player;
 
 typedef struct s_cub
 {
@@ -70,39 +72,39 @@ typedef struct s_cub
 	t_rgb		ceil;
 	t_player	player;
 	char		**map;
-}			t_cub;
+}				t_cub;
 
 // avoid leak
-int			free_struct(t_cub *cub);
+int				free_struct(t_cub *cub);
 
 // parsing
-int			skip_space(char *line, int i);
-t_rgb		get_rgb(char **rgb_tmp);
-void		get_xpm(t_cub *cub);
-t_cub		*fill_struct(t_cub *cub);
-void		flood_fill(int x, int y, char **map, int *ret);
-t_bool		is_one_player(char **map, int i);
-t_bool		is_valid_map(t_cub *cub);
-t_bool		close_or_not(char **map, int x, int y);
-t_bool		is_close_map(char **map, int i, t_cub *cub);
-int			count_line(char *argv);
-char		**loop_copy(int fd, int nb_line);
-char		**copy_map(char *argv);
-t_bool		is_cub(char *map);
-t_cub		*parse_struct(char *map);
-t_player	init_player_struct(t_cub *cub);
+int				skip_space(char *line, int i);
+t_rgb			get_rgb(char **rgb_tmp);
+void			get_xpm(t_cub *cub);
+t_cub			*fill_struct(t_cub *cub);
+void			flood_fill(int x, int y, char **map, int *ret);
+t_bool			is_one_player(char **map, int i);
+t_bool			is_valid_map(t_cub *cub);
+t_bool			close_or_not(char **map, int x, int y);
+t_bool			is_close_map(char **map, int i, t_cub *cub);
+int				count_line(char *argv);
+char			**loop_copy(int fd, int nb_line);
+char			**copy_map(char *argv);
+t_bool			is_cub(char *map);
+t_cub			*parse_struct(char *map);
+t_player		init_player_struct(t_cub *cub);
 
 // render
-void		open_window(t_cub *cub);
-int			handle_keypress(int key, t_cub *cub);
-int			minimap(t_cub *cub);
-void		render_minimap(t_cub *cub);
+void			open_window(t_cub *cub);
+int				handle_keypress(int key, t_cub *cub);
+int				minimap(t_cub *cub);
+void			render_minimap(t_cub *cub);
 
 // render
 
-void		open_window(t_cub *cub);
-int			handle_keypress(int key, t_cub *cub);
+void			open_window(t_cub *cub);
+int				handle_keypress(int key, t_cub *cub);
 
 // debug
 
-void		print_struct(t_cub *cub);
+void			print_struct(t_cub *cub);
--- srcs/rendering/minimap.c
+++ srcs/rendering/minimap.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   minimap.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rbardet- <rbardet-@student.42.fr>          +#+  +:+       +#+        */
+/*   By: hdelacou <hdelacou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/24 20:10:20 by rbardet-          #+#    #+#             */
-/*   Updated: 2025/03/24 23:22:22 by rbardet-         ###   ########.fr       */
+/*   Updated: 2025/03/25 16:02:38 by hdelacou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,10 +18,10 @@ void	draw_square(t_cub *cub, int x, int y, int color)
 	int	j;
 
 	i = 0;
-	while (i < 9)
+	while (i < Tile)
 	{
 		j = 0;
-		while (j < 9)
+		while (j < Tile)
 		{
 			mlx_pixel_put(cub->init_ptr, cub->win_ptr, x + j, y + i, color);
 			j++;
@@ -42,11 +42,11 @@ void	render_minimap(t_cub *cub)
 		while (cub->map[i][j])
 		{
 			if (cub->map[i][j] == '1')
-				draw_square(cub, j * 9, i * 9, 0xFF0000);
+				draw_square(cub, j * Tile, i * Tile, 0xFF0000);
 			else if (cub->map[i][j] == 'D')
-				draw_square(cub, j * 9, i * 9, 0x2BFAFA);
+				draw_square(cub, j * Tile, i * Tile, 0x2BFAFA);
 			else if (cub->map[i][j] == cub->spawn_view)
-				draw_square(cub, j * 9, i * 9, 0x00FF00);
+				draw_square(cub, j * Tile, i * Tile, 0x00FF00);
 			j++;
 		}
 		i++;
