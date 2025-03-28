5136868f4018012b7354dc38639694f7613a53d3
Robin Bardet-Tomczak <rbardet-@student.42lehavre.fr>
2025-03-28 20:04:23 +0100

    Fix: Movement

--- srcs/rendering/movement.c
+++ srcs/rendering/movement.c
@@ -6,7 +6,7 @@
 /*   By: rbardet- <rbardet-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/26 22:51:32 by rbardet-          #+#    #+#             */
-/*   Updated: 2025/03/28 19:30:39 by rbardet-         ###   ########.fr       */
+/*   Updated: 2025/03/28 20:03:47 by rbardet-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,73 +23,93 @@ t_bool	move_front(t_cub *cub)
 {
 	double	tmp_x;
 	double	tmp_y;
+	t_bool	move;
 
+	move = FALSE;
 	tmp_x = cub->player.pos_x;
 	tmp_y = cub->player.pos_y;
 	cub->player.pos_x += cub->player.dir_x * MOVE_SPEED;
+	if (!check_collision(cub, cub->player.pos_x, cub->player.pos_y))
+		cub->player.pos_x = tmp_x;
+	else
+		move = TRUE;
 	cub->player.pos_y += cub->player.dir_y * MOVE_SPEED;
 	if (!check_collision(cub, cub->player.pos_x, cub->player.pos_y))
-	{
-		cub->player.pos_x = tmp_x;
 		cub->player.pos_y = tmp_y;
-		return (FALSE);
-	}
-	return (TRUE);
+	else
+		move = TRUE;
+
+	return (move);
 }
+
 
 t_bool	move_back(t_cub *cub)
 {
 	double	tmp_x;
 	double	tmp_y;
+	t_bool	move;
 
+	move = FALSE;
 	tmp_x = cub->player.pos_x;
 	tmp_y = cub->player.pos_y;
 	cub->player.pos_x -= cub->player.dir_x * MOVE_SPEED;
-	cub->player.pos_y -= cub->player.dir_y * MOVE_SPEED;
-	if (!check_collision(cub, cub->player.pos_x, cub->player.pos_y))
-	{
+	if (check_collision(cub, cub->player.pos_x, cub->player.pos_y))
+		move = TRUE;
+	else
 		cub->player.pos_x = tmp_x;
+	cub->player.pos_y -= cub->player.dir_y * MOVE_SPEED;
+	if (check_collision(cub, cub->player.pos_x, cub->player.pos_y))
+		move = TRUE;
+	else
 		cub->player.pos_y = tmp_y;
-		return (FALSE);
-	}
-	return (TRUE);
+	return (move);
 }
+
 
 t_bool	move_left(t_cub *cub)
 {
 	double	tmp_x;
 	double	tmp_y;
+	t_bool	move;
 
+	move = FALSE;
 	tmp_x = cub->player.pos_x;
 	tmp_y = cub->player.pos_y;
-	cub->player.pos_x += cub->player.dir_y * MOVE_SPEED;
-	cub->player.pos_y += cub->player.dir_x * MOVE_SPEED;
+	cub->player.pos_x -= cub->player.plane_x * MOVE_SPEED;
 	if (!check_collision(cub, cub->player.pos_x, cub->player.pos_y))
-	{
 		cub->player.pos_x = tmp_x;
+	else
+		move = TRUE;
+	cub->player.pos_y -= cub->player.plane_y * MOVE_SPEED;
+	if (!check_collision(cub, cub->player.pos_x, cub->player.pos_y))
 		cub->player.pos_y = tmp_y;
-		return (FALSE);
-	}
-	return (TRUE);
+	else
+		move = TRUE;
+	return (move);
 }
 
 t_bool	move_right(t_cub *cub)
 {
 	double	tmp_x;
 	double	tmp_y;
+	t_bool	move;
 
+	move = FALSE;
 	tmp_x = cub->player.pos_x;
 	tmp_y = cub->player.pos_y;
-	cub->player.pos_x -= cub->player.dir_y * MOVE_SPEED;
-	cub->player.pos_y -= cub->player.dir_x * MOVE_SPEED;
+	cub->player.pos_x += cub->player.plane_x * MOVE_SPEED;
 	if (!check_collision(cub, cub->player.pos_x, cub->player.pos_y))
-	{
 		cub->player.pos_x = tmp_x;
+	else
+		move = TRUE;
+	cub->player.pos_y += cub->player.plane_y * MOVE_SPEED;
+	if (!check_collision(cub, cub->player.pos_x, cub->player.pos_y))
 		cub->player.pos_y = tmp_y;
-		return (FALSE);
-	}
-	return (TRUE);
+	else
+		move = TRUE;
+	return (move);
 }
+
 
 t_bool	rotate_right(t_cub *cub)
 {
