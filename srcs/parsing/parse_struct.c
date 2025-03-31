63e5a2ec63d2584450cfd143f495d1261fd84080
Robin Bardet-Tomczak <rbardet-@student.42lehavre.fr>
2025-03-31 22:35:26 +0200

    Fix: norminette

--- srcs/parsing/parse_struct.c
+++ srcs/parsing/parse_struct.c
@@ -6,7 +6,7 @@
 /*   By: rbardet- <rbardet-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/20 00:30:31 by rbardet-          #+#    #+#             */
-/*   Updated: 2025/03/31 22:22:34 by rbardet-         ###   ########.fr       */
+/*   Updated: 2025/03/31 22:34:49 by rbardet-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -77,24 +77,10 @@ static char	**sort_arg(char **tab, t_cub *cub)
 	return (sorted);
 }
 
-// fill the struct with all the data for the rendering
-// add in the xpm , rgb code for floor and ceilling
-// and a copy of the map
-static t_cub	*fill_struct(t_cub *cub)
+static t_cub	*fill_struct_utils(t_cub *cub, int has_door, char **tmp)
 {
 	char	**rgb_tmp;
-	char	**tmp;
-	int		has_door;
 
-	has_door = 0;
-	if (cub->has_door == TRUE)
-		has_door += 1;
-	tmp = sort_arg(cub->map, cub);
-	if ((tab_size(tmp) != 6 && cub->has_door == FALSE) || (tab_size(tmp) != 7 && cub->has_door == TRUE))
-		return (free_struct(cub), free_tab(tmp), NULL);
-	load_png(cub, tmp);
-	if (!cub->no_xpm || !cub->so_xpm || !cub->ea_xpm || !cub->we_xpm || (!cub->do_xpm && cub->has_door == TRUE))
-		return (free_struct(cub), free_tab(tmp), NULL);
 	rgb_tmp = ft_split(tmp[0], ',');
 	if (!rgb_tmp || tab_size(rgb_tmp) != 3)
 		return (free_tab(rgb_tmp), free_tab(tmp), free_struct(cub), NULL);
@@ -109,6 +95,29 @@ static t_cub	*fill_struct(t_cub *cub)
 	cub->map = copy_and_check_map(cub);
 	if (!cub->map)
 		return (free_struct(cub), NULL);
+	return (cub);
+}
+
+// fill the struct with all the data for the rendering
+// add in the xpm , rgb code for floor and ceilling
+// and a copy of the map
+static t_cub	*fill_struct(t_cub *cub)
+{
+	char	**tmp;
+	int		has_door;
+
+	has_door = 0;
+	if (cub->has_door == TRUE)
+		has_door += 1;
+	tmp = sort_arg(cub->map, cub);
+	if ((tab_size(tmp) != 6 && cub->has_door == FALSE) || (tab_size(tmp) != 7 && cub->has_door == TRUE))
+		return (free_struct(cub), free_tab(tmp), NULL);
+	load_png(cub, tmp);
+	if (!cub->no_xpm || !cub->so_xpm || !cub->ea_xpm || !cub->we_xpm || (!cub->do_xpm && cub->has_door == TRUE))
+		return (free_struct(cub), free_tab(tmp), NULL);
+	cub = fill_struct_utils(cub, has_door, tmp);
+	if (!cub)
+		return (NULL);
 	if (cub->floor.red == -1 || cub->floor.green == -1
 		|| cub->floor.blue == -1 || cub->ceil.red == -1
 		|| cub->ceil.green == -1 || cub->ceil.blue == -1)
